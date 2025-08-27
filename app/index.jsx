125a33f933244b1eb8b17474277fed5a5e1972d2
medallorey2004 <medallorey27@gmail.com>
2025-08-27 15:38:09 +0800

    Firebase Configuration

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Quicky</Text>
+      <Text>Welcome to Quicky
+        V J P G O A L S T R A C K E R
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
