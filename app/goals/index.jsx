c134e587fe309e81dfeca682cdf429805040289c
valjosephmantuhac-collab <valjosephmantuhac@gmail.com>
2025-09-03 16:41:42 +0800

    Create Goals

--- app/goals/index.jsx
+++ app/goals/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        M Y 
+        M Y G O A L S
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
